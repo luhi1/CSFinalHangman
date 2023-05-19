483661d67fb7ee6a9ae07e253e184ab6534126c9
micha <michaelborov2007@gmail.com>
2023-05-18 20:54:13 -0700

    Added a reminder for presentation day.

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        //TODO Change to school computer IP
         Host myHost = new Host("127.0.0.1", 6666);
         Boolean winOrLoss = myHost.startGame();
         String menuChoice = "";

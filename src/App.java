c5b4454472b2d613debec415ee8630bd090ada25
Borov <Michael.378826@ad.ccsd.net>
2023-05-19 07:27:11 -0700

    fin

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         //TODO Change to school computer IP
-        Host myHost = new Host("127.0.0.1", 6666);
+        Host myHost = new Host("10.129.210.117", 6666);
         Boolean winOrLoss = myHost.startGame();
         String menuChoice = "";
         Scanner menuReader = new Scanner(System.in);
--- src/Host.java
+++ src/Host.java
@@ -11,37 +11,13 @@ public class Host{
     private String guess;
     private Socket clientSocket;
 
-private ObjectOutputStream gameOut;
+    private ObjectOutputStream gameOut;
     private ObjectInputStream gameIn;
     private PrintWriter messagesOut;
     private BufferedReader messagesIn;
     private int chain;
-    
-        public int getChain() {
-                return chain;
-        }
-
-        public void resetChain() {
-                this.chain = 0;
-        }
-
-        public void incrementChain() {
-                this.chain++;
-        }
-
-        public void sendMessage(String m){
-                messagesOut.println(m);
-        }
-
-        public BufferedReader getInputStream(){
-                return messagesIn;
-        }
-
-        public Socket getClientSocket() {
-                return clientSocket;
-        }
-
-public Host(){
+     
+    public Host(){
         this.guess = "";
     }
     
@@ -50,34 +26,56 @@ public Host(){
         chain = 0;
         connect(serverIP, serverPort);
     }
-    
-    //Important, IP's are strings!
-    public void connect(String ip, int port){
-            try {
-                    clientSocket = new Socket(ip, port);
-                    gameOut = new ObjectOutputStream(clientSocket.getOutputStream());
-                    gameIn = new ObjectInputStream(clientSocket.getInputStream());
 
-                    messagesOut = new PrintWriter(clientSocket.getOutputStream(), true);
-                    messagesIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            } catch (IOException e) {
-                    e.printStackTrace();
-            }
+    public int getChain() {
+        return chain;
+    }
+
+    public void resetChain() {
+        this.chain = 0;
+    }
+
+    public void incrementChain() {
+        this.chain++;
+    }
+
+    public void sendMessage(String m){
+        messagesOut.println(m);
+    }
+
+    public BufferedReader getInputStream(){
+        return messagesIn;
+    }
+
+    public Socket getClientSocket() {
+        return clientSocket;
+    }
+
+    public void connect(String ip, int port){
+        try {
+                clientSocket = new Socket(ip, port);
+                gameOut = new ObjectOutputStream(clientSocket.getOutputStream());
+                gameIn = new ObjectInputStream(clientSocket.getInputStream());
+
+                messagesOut = new PrintWriter(clientSocket.getOutputStream(), true);
+                messagesIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
 
     }
 
     public boolean startGame(){
-            messagesOut.println("newGame");
-            //Println = send the message, scanner reading is just like user input!
-            Game resp;
-            try {
-                    resp = (Game) gameIn.readObject();
-                    return this.currentGame(resp);
-            } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-            }
-            return false;
+        messagesOut.println("newGame");
+        //Println = send the message, scanner reading is just like user input!
+        Game resp;
+        try {
+                resp = (Game) gameIn.readObject();
+                return this.currentGame(resp);
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
+        return false;
     }
 
     public boolean currentGame(Game g) throws Exception{
@@ -89,6 +87,7 @@ public Host(){
         g.displayScreen();
 
         Scanner guessReader = new Scanner(System.in);
+        //The scanner is closed in App.java, not a memory leak trust.
         System.out.println("Type here: ");
         this.guess = guessReader.nextLine();
         g.checkGuess(guess);
